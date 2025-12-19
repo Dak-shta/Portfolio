ecce9ac6d62048a5cae8a01c77a05513b7567625
Dak-shta <dakshtakulshreshtha26@gmail.com>
2025-12-19 11:11:25 +0530

    hey

---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import {React} from "react"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
+export default Navbar
